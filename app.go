c57ce0c60a9ba7eea70eef31df11e237a35679c7
herdian tristyantono <herdiantristyantono69@gmail.com>
2020-04-08 18:09:09 +0700

    Project Hacktiv8 GoLang

--- app.go
+++ app.go
@@ -1,31 +1,28 @@
 package main
 
 import (
-	user2 "bitbucket.org/Hacktive8/FinalProjectGO/src/user"
+	user2 "ProjectGoLang/src/user"
 	"fmt"
-	"github.com/gorilla/sessions"
 	"html/template"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/sessions"
 )
 
 var (
 	// key must be 16, 24 or 32 bytes long (AES-128, AES-192 or AES-256)
-	key = []byte("super-secret-key")
+	key   = []byte("super-secret-key")
 	store = sessions.NewCookieStore(key)
 )
 
-func main()  {
+func main() {
 	fmt.Println("hello")
 	Template := template.Must(template.ParseGlob("files/var/templates/*"))
 	user := user2.New(Template)
 
 	http.HandleFunc("/", user.HomeUser)
 	http.HandleFunc("/homelogged", user.HomeUserLogged)
-	http.HandleFunc("/about", user.AboutUser)
-	http.HandleFunc("/aboutlogged", user.AboutUserLogged)
-	http.HandleFunc("/contact", user.ContactUser)
-	http.HandleFunc("/contactlogged", user.ContactUserLogged)
 	http.HandleFunc("/articles", user.ArticlesUser)
 	http.HandleFunc("/articles/add", user.AddArticlesUser)
 	http.HandleFunc("/articles/edit", user.EditArticlesUser)
@@ -36,9 +33,6 @@ func main()  {
 	http.HandleFunc("/login", user.LoginUser)
 	http.HandleFunc("/logout", user.LogoutUser)
 	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets"))))
-
-
-
 
 	err := http.ListenAndServe(":9090", nil)
 	if err != nil {
